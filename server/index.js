852551c5a526618cb91c45827156e174e9279125
roh <rodrigosousa1105@gmail.com>
2024-03-06 22:15:41 +0000

    Cors added

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 
 const usersRouter = require("./routes/users");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 mongoose
     .connect(process.env.MONGODB)
