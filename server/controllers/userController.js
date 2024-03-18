9a045d7ead58f93f3b663554656671ae33ddc2b1
roh <rodrigosousa1105@gmail.com>
2024-03-18 18:46:20 +0000

    User routes updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,58 @@
 const User = require("../models/User");
 require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+	try {
+		const { firstName, lastName, email, password } = req.body;
+
+		const existingUser = await User.findOne({ email });
+		if (existingUser) {
+			return res.status(400).json({ message: "Email already exists" });
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		const newUser = new User({
+			firstName,
+			lastName,
+			email,
+			password: hashedPassword,
+		});
+		await newUser.save();
+
+		res.status(201).json({ message: "User registered successfully" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: "Registration failed" });
+	}
+};
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+
+		const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, {
+			expiresIn: "30d",
+		});
+
+        res.json({ accessToken: accessToken });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Login failed" });
+    }
+};
 
 const getLoggedInUserDetails = async (req, res) => {
 	try {
@@ -18,5 +71,7 @@ const getLoggedInUserDetails = async (req, res) => {
 };
 
 module.exports = {
+	register,
+	login,
 	getLoggedInUserDetails,
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const authenticateToken = require("../middlewares/authenticateToken");
 const userController = require("../controllers/userController");
 
+router.post("/login", userController.login);
+router.post("/register", userController.register);
 router.get("/me", authenticateToken, userController.getLoggedInUserDetails);
 
 module.exports = router;
